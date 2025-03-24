049485929fce80f67fe2973a93b31a538ca93c7d
Anshuman Gupta <grario@Anshumans-MacBook-Air.local>
2025-03-25 03:13:36 +0530

    adding reset btn and fixing multisend

--- frontend/src/send.tsx
+++ frontend/src/send.tsx
@@ -1,17 +1,21 @@
 import { useState } from 'react';
 import './send.css';
+import resetImg from './assets/reset-svgrepo-com.svg';
 
 const Send = () => {
   const [message, setMessage] = useState('');
   const [uniqueCode, setUniqueCode] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
 
   const handleSend = async () => {
+    setUniqueCode('');
     if (message === '') {
       alert('Please enter a message before sending.');
       return; 
     }
 
     try {
+      setIsLoading(true);
       const response = await fetch('http://localhost:3000/api/send', {
         method: 'POST',
         headers: {
@@ -26,20 +30,30 @@ const Send = () => {
 
       const data = await response.json();
       setUniqueCode(data.uniqueCode);
+      setIsLoading(false);
     } catch (error) {
-      console.error('Error:', error);
       alert('Failed to send message');
+      setIsLoading(false);
     }
+  };
+
+  const handleReset = () => {
+    setMessage('');
   };
 
   return (
     <div className="container">
+      <button onClick={handleReset} className='resetbtn'>
+        <img src={resetImg} alt="reset" className="reset-icon" />
+      </button>
       <textarea className='input'
         value={message}
         onChange={(e) => setMessage(e.target.value)}
         placeholder="Enter your message"
       />
-      <button onClick={handleSend}>Send</button>
+      <button onClick={handleSend} disabled={isLoading}>
+        {isLoading ? 'Sending...' : 'Send'}
+      </button>
       {uniqueCode && (
         <div className="code-received">
           <p>Your Code: {uniqueCode}</p>
